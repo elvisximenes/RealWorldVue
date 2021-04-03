19bd827208215ee79ea7898e96dffa52751c52e7
Elvis Ximenes <elvis.nacarras@gmail.com>
2021-04-03 21:42:23 +0900

    first commit

---
+++ src/views/EventDetails.vue
@@ -0,0 +1,29 @@
+<template>
+    <div v-if="event">
+        <h4>{{ event.title }}</h4>
+        <p>@{{ event.time }} on {{ event.date }}</p>
+        <p>{{event.description}}</p>
+    </div>
+</template>
+<script>
+import EventService from "@/services/EventService";
+    export default {
+        name:"EventDetails",
+        data() {
+            return {
+                event:null,
+            }
+        },
+        props:['id'],
+        created() {
+            EventService.getEvent(this.id)
+            .then((response)=> {
+                this.event = response.data
+            })
+            .catch((error)=> {
+                console.log(error)
+            });
+        }
+
+    }
+</script>
